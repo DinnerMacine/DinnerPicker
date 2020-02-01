e48c5f440b8686fad86e5e4556283bd1dfaa3f81
unknown <mile3880@gmail.com>
2020-02-01 20:34:18 +0900

    [add] empty project

---
+++ DinnerPicker/app/src/main/java/dinnerMachine/dinnerPicker/MainActivity.kt
@@ -0,0 +1,12 @@
+package dinnerMachine.dinnerPicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
