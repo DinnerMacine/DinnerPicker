ca5c74c0b1f07b2788279bebea074d8f9580a557
unknown <mile3880@gmail.com>
2020-02-13 20:51:21 +0900

    [push] clean push

--- DinnerPicker/app/src/main/java/dinnerMachine/dinnerPicker/MainActivity.kt
+++ DinnerPicker/app/src/main/java/dinnerMachine/dinnerPicker/MainActivity.kt
@@ -1,12 +1,31 @@
 package dinnerMachine.dinnerPicker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.transition.Scene
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val bottomTap = findViewById<BottomNavigationView>(R.id.tab)
+        bottomTap.setOnNavigationItemSelectedListener(this)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.tab_map -> { return true }
+            R.id.tab_home -> { return true }
+            R.id.tab_setting -> { return true }
+        }
+        return false
+    }
+
 }
---
+++ DinnerPicker/app/src/main/java/dinnerMachine/dinnerPicker/Setting.kt
@@ -0,0 +1,20 @@
+package dinnerMachine.dinnerPicker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Setting : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.layout_tab_setting, container, false)
+
+        return view
+    }
+}
